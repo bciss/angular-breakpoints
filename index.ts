3e7084dde1c96f1cde301a13134110b9965d1f9c
babacar <babacar.ciss@it-ce.fr>
2018-03-07 10:34:53 +0100

    lint

--- index.ts
+++ index.ts
@@ -41,6 +41,108 @@ const FALLBACK_BREAKPOINT = {
 
 export const BREAKPOINTS_CONFIG = new InjectionToken<BreakpointConfig>('breakpoints.config');
 
+@Injectable()
+export class BreakpointsService  {
+
+    private lastBreakpoint: string = null;
+    private breakpoints: BreakpointConfig = defaultBreakpoints;
+    private changesSubject: BehaviorSubject<BreakpointEvent>;
+    private subscription: Subscription;
+
+    changes: Observable<BreakpointEvent>;
+    resize: Observable<WindowSize>;
+
+    constructor (private ngZone, breakpoints?: BreakpointConfig) {
+
+        this.setBreakpoints(breakpoints);
+
+        this.resize = Observable.fromEvent(window, 'resize').map(this.getWindowSize).distinctUntilChanged();
+
+        const initialBreakpoint = this.getBreakpoint(window.innerWidth);
+        this.changesSubject = new BehaviorSubject<BreakpointEvent>(this.getBreakpointEvent(initialBreakpoint));
+        this.changes = this.changesSubject.distinctUntilChanged((x, y) => x.name === y.name);
+        this.subscribe();
+    }
+
+    // Unsubscribe to the resize event
+    public unsubscribe () {
+
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+            this.subscription = null;
+        }
+    }    
+
+    // Subscribe to the resize event
+    public subscribe () {
+
+        if (this.subscription) {
+            return;
+        }
+
+        // Make sure resize event doesn't trigger change detection by running outside of angular zone
+        this.ngZone.runOutsideAngular(() => {
+
+            this.subscription = this.resize.subscribe((size: WindowSize) => {
+
+                const breakpoint: string = this.getBreakpoint(size.width);
+
+                if (breakpoint === this.lastBreakpoint) {
+                    return;
+                }
+
+                this.lastBreakpoint = breakpoint;
+
+                // Emitting back in angular zone
+                this.ngZone.run(() => {
+                    this.changesSubject.next(this.getBreakpointEvent(breakpoint));
+                });
+            });
+        });
+    }
+
+    // Sets the customized breakpoints
+    private setBreakpoints (breakpoints?: BreakpointConfig) {
+
+        if (breakpoints) {
+            this.breakpoints = breakpoints;
+        }
+    }
+
+
+    // Returns a breakpoint event, with the fallback breakpoint if none were found
+    private getBreakpointEvent (name: string): BreakpointEvent {
+
+        if (!name) {
+            return { name: 'default', breakpoint: FALLBACK_BREAKPOINT, size: this.getWindowSize() };
+        } else {
+            return { name: name, breakpoint: this.breakpoints[name], size: this.getWindowSize() };
+        }
+    }
+
+    // Returns the current window size
+    private getWindowSize (): WindowSize {
+
+        return { width: window.innerWidth, height: window.innerHeight };
+    }
+
+    // Returns the first breakpoint that match the current size
+    private getBreakpoint (currentSize: number): string {
+
+        const keys = Object.keys(this.breakpoints);
+        for (const key of keys) {
+            const value = this.breakpoints[key];
+            const min = value.min || 0;
+            const max = value.max || Number.MAX_SAFE_INTEGER;
+
+            if (currentSize >= min && currentSize < max) {
+                return key;
+            }
+        }
+        return null;
+    }
+}
+
 export function breakpointsFactory (ngZone: NgZone, breakpoints: BreakpointConfig) {
     return new BreakpointsService(ngZone, breakpoints)
 }
@@ -59,111 +161,3 @@ export function breakpointsProvider (breakpoints: BreakpointConfig = defaultBrea
         }
     ];
 }
-
-
-@Injectable()
-export class BreakpointsService  {
-    
-    private lastBreakpoint: string = null;
-    private breakpoints: BreakpointConfig = defaultBreakpoints;
-    private changesSubject: BehaviorSubject<BreakpointEvent>;
-    private subscription: Subscription;
-    
-    changes: Observable<BreakpointEvent>;
-    resize: Observable<WindowSize>;
-    
-    constructor (private ngZone, breakpoints?: BreakpointConfig) {
-        
-        this.setBreakpoints(breakpoints);
-        
-        this.resize = Observable.fromEvent(window, 'resize').map(this.getWindowSize).distinctUntilChanged();
-        
-        const initialBreakpoint = this.getBreakpoint(window.innerWidth);
-        this.changesSubject = new BehaviorSubject<BreakpointEvent>(this.getBreakpointEvent(initialBreakpoint));
-        this.changes = this.changesSubject.distinctUntilChanged((x, y) => x.name === y.name);
-        this.subscribe();
-    }
-
-    // Unsubscribe to the resize event
-    public unsubscribe () {
-        
-        if (this.subscription) {
-            this.subscription.unsubscribe();
-            this.subscription = null;
-        }
-    }    
-    
-    // Subscribe to the resize event
-    public subscribe () {
-        
-        if (this.subscription) {
-            return;
-        }
-        
-        // Make sure resize event doesn't trigger change detection by running outside of angular zone
-        this.ngZone.runOutsideAngular(() => {
-            
-            this.subscription = this.resize.subscribe((size: WindowSize) => {
-                
-                const breakpoint: string = this.getBreakpoint(size.width);
-                
-                if (breakpoint === this.lastBreakpoint) {
-                    return;
-                }
-                
-                this.lastBreakpoint = breakpoint;
-                
-                // Emitting back in angular zone
-                this.ngZone.run(() => {
-                    this.changesSubject.next(this.getBreakpointEvent(breakpoint));
-                });
-            });
-        });
-        
-    }
-    
-    // Sets the customized breakpoints
-    private setBreakpoints (breakpoints?: BreakpointConfig) {
-        
-        if (breakpoints) {
-            this.breakpoints = breakpoints;
-        }
-    }
-    
-    
-    // Returns a breakpoint event, with the fallback breakpoint if none were found
-    private getBreakpointEvent (name: string): BreakpointEvent {
-        
-        if (!name) {
-            return { name: 'default', breakpoint: FALLBACK_BREAKPOINT, size: this.getWindowSize() };
-        }
-        else {
-            return { name: name, breakpoint: this.breakpoints[name], size: this.getWindowSize() };
-        }
-    }
-    
-    // Returns the current window size
-    private getWindowSize (): WindowSize {
-        
-        return { width: window.innerWidth, height: window.innerHeight };
-    }
-    
-    
-    // Returns the first breakpoint that match the current size
-    private getBreakpoint (currentSize: number): string {
-        
-        const keys = Object.keys(this.breakpoints);
-        for (const key of keys) {
-            const value = this.breakpoints[key];
-            const min = value.min || 0;
-            const max = value.max || Number.MAX_SAFE_INTEGER;
-            
-            if (currentSize >= min && currentSize < max) {
-                return key;
-            }
-        }
-        
-        return null;
-    }
-}
-
